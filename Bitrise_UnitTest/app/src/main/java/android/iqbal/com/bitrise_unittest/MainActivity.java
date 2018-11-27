64c5cbc81cb4647c68219f8d3ee7a0bcd8c89c07
BehzadIqbal <iqbalbehzad@gmail.com>
2018-11-27 22:01:24 +0100

    mijn eerste commit

---
+++ Bitrise_UnitTest/app/src/main/java/android/iqbal/com/bitrise_unittest/MainActivity.java
@@ -0,0 +1,13 @@
+package android.iqbal.com.bitrise_unittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
